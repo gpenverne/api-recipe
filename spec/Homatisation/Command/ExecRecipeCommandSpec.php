c48492bac95d2b9f21dd964d9ff70fd0e870b612
gpenverne <gpenverne@gmail.com>
2016-12-10 16:44:11 +0100

    fix specs

--- spec/Homatisation/Command/ExecRecipeCommandSpec.php
+++ spec/Homatisation/Command/ExecRecipeCommandSpec.php
@@ -2,17 +2,11 @@
 
 namespace spec\Homatisation\Command;
 
-use Homatisation\Command\SampleCommand;
 use PhpSpec\ObjectBehavior;
 use Symfony\Component\Console\Command\Command;
 
-class SampleCommandSpec extends ObjectBehavior
+class ExecRecipeCommandSpec extends ObjectBehavior
 {
-    public function it_is_initializable()
-    {
-        $this->shouldHaveType(SampleCommand::class);
-    }
-
     public function it_is_a_command()
     {
         $this->shouldBeAnInstanceOf(Command::class);
