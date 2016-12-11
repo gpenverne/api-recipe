bf53ba00962bc82f190e5d8f477f6b7f28236f6d
gpenverne <gpenverne@gmail.com>
2016-12-11 11:40:39 +0100

    Using images from host

---
+++ cordova/platforms/android/assets/www/js/android/cordovaApp.js
@@ -0,0 +1,16 @@
+var cordovaApp = {
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+    }
+};
+
+cordovaApp.initialize();
---
+++ cordova/platforms/android/assets/www/js/android/parametersController.js
@@ -0,0 +1,28 @@
+app.controller('parametersCtrl', function ($scope, $http, $timeout, $window) {
+    var permanentStorage = window.localStorage;
+
+    $scope.hostName = window.localStorage.getItem("host");
+    if (!$scope.hostName) {
+        $scope.$parent.parametersVisible = 1;
+    }
+    hostApi = $scope.hostName;
+
+    if (!$scope.hostName) {
+        $scope.hostName = 'http://127.0.0.1';
+    }
+
+    $scope.setHost = function(host) {
+        host = 'http://'+host.replace('http://', '');
+        window.localStorage.setItem("host", host);
+        $scope.hostName = host;
+        hostApi = $scope.hostName;
+        $scope.close();
+
+    };
+
+
+    $scope.close = function()
+    {
+        $scope.$parent.parametersVisible = 0;
+    };
+});
