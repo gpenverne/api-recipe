6019fe7d8f17565a5399f1d0265ceca48cd42ada
gpenverne <gpenverne@gmail.com>
2016-12-13 09:20:53 +0100

    Launch android app action without waiting the recipe execution end

--- cordova/platforms/android/assets/www/assets/app.js
+++ cordova/platforms/android/assets/www/assets/app.js
@@ -89,28 +89,43 @@ app.controller('appCtrl', function ($scope, $http, $timeout, $window, currentTag
     $scope.execRecipe = function(recipe){
         recipe.runing = true;
         recipe.error = false;
+        var actions = new Array;
+
         $http.get(hostApi+recipe.url).then(function(r){
             recipe.runing = false;
-            var actions = r.data.actions;
-            for (var action in actions) {
-                if (device.platform == 'Android' || true) {
-                    var actionsInfos = action.split(':');
-                    var provider = actionsInfos[0];
-                    var method = actionsInfos[1];
-                    if ('android' == provider && 'openApp' == method) {
-                        try {
-                            var arg = actionsInfos[2];
-                            startApp.set({
-                                "package": arg
-                            }).start();
-                        } catch (e) {}
-                    }
-                }
-            }
         }, function(){
             recipe.runing = false;
             recipe.error = true;
         });
+
+
+        if (device.platform == 'Android') {
+            if (recipe.state == 'on') {
+                if (typeof recipe.actions.off != 'undefined') {
+                    actions = actions.concat(recipe.actions.off);
+                }
+            } else {
+                if (typeof recipe.actions.on != 'undefined') {
+                    actions = actions.concat(recipe.actions.on);
+                }
+            }
+            if (typeof recipe.actions.each_time != 'undefined') {
+                actions = actions.concat(recipe.actions.each_time);
+            }
+
+
+            for (var i=0; i < actions.length; i++) {
+                var action = actions[i];
+                var actionsInfos = action.split(':');
+                if (actionsInfos[0] == 'android' && actionsInfos[1] == 'openApp') {
+                    try {
+                        startApp.set({
+                            "package": actionsInfos[2]
+                        }).start();
+                    } catch (e) {}
+                }
+            }
+        }
 
         if (typeof recipe.external != 'undefined') {
             $window.open(recipe.external);
