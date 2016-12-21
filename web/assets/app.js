d87fbc85f1db434e73f84da657a4a15ac8179b8f
gpenverne <gpenverne@gmail.com>
2016-12-21 09:16:13 +0100

    priorize host load

--- web/assets/app.js
+++ web/assets/app.js
@@ -16,12 +16,13 @@ var voiceManager = {
     listen: function(callback){},
     say: function(text){
         var msg = new SpeechSynthesisUtterance();
+        var voices = window.speechSynthesis.getVoices();
         msg.volume = 1; // 0 to 1
         msg.rate = 1; // 0.1 to 10
         msg.pitch = 2; //0 to 2
         msg.text = text;
         msg.lang = 'fr-FR';
-
+        console.log('say text');
         speechSynthesis.speak(msg);
     }
 };
