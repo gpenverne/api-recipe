3de9d9338c0fc4d378c341e52424daec7f955471
gpenverne <gpenverne@gmail.com>
2016-12-21 09:15:21 +0100

    priorize host load

--- web/assets/app.js
+++ web/assets/app.js
@@ -16,9 +16,6 @@ var voiceManager = {
     listen: function(callback){},
     say: function(text){
         var msg = new SpeechSynthesisUtterance();
-        var voices = window.speechSynthesis.getVoices();
-        msg.voice = voices[10]; // Note: some voices don't support altering params
-        msg.voiceURI = 'native';
         msg.volume = 1; // 0 to 1
         msg.rate = 1; // 0.1 to 10
         msg.pitch = 2; //0 to 2
