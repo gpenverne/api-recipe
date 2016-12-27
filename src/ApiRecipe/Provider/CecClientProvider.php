abf29abaf16da6c3b024244fa8181e0f45177ed1
gpenverne <gpenverne@gmail.com>
2016-12-27 08:27:01 +0100

    CEC premade commands

--- src/ApiRecipe/Provider/CecClientProvider.php
+++ src/ApiRecipe/Provider/CecClientProvider.php
@@ -2,6 +2,8 @@
 
 namespace ApiRecipe\Provider;
 
+use Doctrine\Common\Inflector\Inflector;
+
 class CecClientProvider implements ProviderInterface
 {
     use HydratorTrait;
@@ -18,6 +20,11 @@ class CecClientProvider implements ProviderInterface
     {
         return [
             'command',
+            'turnOn',
+            'turnOff',
+            'volumeUp',
+            'volumeDown',
+            'setActiveSource',
         ];
     }
 
@@ -26,8 +33,112 @@ class CecClientProvider implements ProviderInterface
      *
      * @return bool
      */
-    public function command($command)
+    public function command($command, $device = null)
     {
-        return shell_exec(sprintf('echo %s | %s -s -d 1', $command, $this->binary));
+        if (null === $device) {
+            return shell_exec(sprintf('echo %s | %s -s -d 1', $command, $this->binary));
+        }
+
+        $device = $this->getDevice($device);
+
+        return shell_exec(sprintf('echo %s %s | %s -s -d 1', $command, $device->address, $this->binary));
+    }
+
+    /**
+     * @param string $deviceName
+     *
+     * @return bool
+     */
+    public function turnOn($deviceName = null)
+    {
+        return $this->command('on', $deviceName);
+    }
+
+    /**
+     * @param string $deviceName
+     *
+     * @return bool
+     */
+    public function turnOff($deviceName = null)
+    {
+        return $this->command('standby', $deviceName);
+    }
+
+    /**
+     * @param string $deviceName
+     *
+     * @return bool
+     */
+    public function volumeUp($deviceName = null)
+    {
+        return $this->command('volup');
+    }
+
+    /**
+     * @param string $deviceName
+     *
+     * @return bool
+     */
+    public function volumeDown($deviceName = null)
+    {
+        return $this->command('voldown');
+    }
+
+    /**
+     * @param string $deviceName
+     *
+     * @return bool
+     */
+    public function setActiveSource($deviceName = null)
+    {
+        return $this->command('spl', $deviceName);
+    }
+
+    /**
+     * @param string $deviceName
+     *
+     * @return \stdClass
+     */
+    private function getDevice($deviceName)
+    {
+        $regexp = sprintf('/%s/', strtolower($deviceName));
+        $devices = $this->getDevices();
+        foreach ($devices as $device) {
+            if (preg_match($regexp, $device->osdString)) {
+                return $device;
+            }
+        }
+
+        throw new \Exception(sprintf('Device %s not found', $deviceName));
+    }
+
+    /**
+     * @return \stdClass[]
+     */
+    private function getDevices()
+    {
+        $returnDevices = [];
+        $rawDevicesList = $this->command('scan');
+        $devices = explode('device #', $rawDevicesList);
+        unset($devices[0]);
+        foreach ($devices as $device) {
+            $class = new \stdClass();
+            $lines = explode("\n", $device);
+            array_pop($lines);
+            array_shift($lines);
+            foreach ($lines as $line) {
+                $values = array_values(explode(':', $line));
+                if (!isset($values[1]) || $values[0] == '') {
+                    continue;
+                }
+
+                list($key, $value) = $values;
+                $key = Inflector::camelize($key);
+                $class->$key = strtolower(trim($value));
+            }
+            $returnDevices[] = $class;
+        }
+
+        return $returnDevices;
     }
 }
