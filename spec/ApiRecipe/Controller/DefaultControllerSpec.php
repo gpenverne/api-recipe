14c6a795fa27da22c8a290e7a47cde0306dfe754
gpenverne <gpenverne@gmail.com>
2017-02-02 21:23:35 +0100

    fix minor specs

--- spec/ApiRecipe/Controller/DefaultControllerSpec.php
+++ spec/ApiRecipe/Controller/DefaultControllerSpec.php
@@ -5,10 +5,16 @@ namespace spec\ApiRecipe\Controller;
 use ApiRecipe\Controller\DefaultController;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultControllerSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    public function let(Request $request)
+    {
+        $this->beConstructedWith($request);
+    }
+    
+    public function it_is_initializable()
     {
         $this->shouldHaveType(DefaultController::class);
     }
