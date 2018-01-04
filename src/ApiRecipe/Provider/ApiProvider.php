bf3dffd2588ce661d6360ccd96c7526bab3cf2c1
gpenverne <gpenverne@gmail.com>
2018-01-04 05:45:57 +0100

    async using shell exec

--- src/ApiRecipe/Provider/ApiProvider.php
+++ src/ApiRecipe/Provider/ApiProvider.php
@@ -45,7 +45,11 @@ class ApiProvider implements ProviderInterface
     {
         $consolePath = realpath(sprintf('%s/../../bin/console', __DIR__));
         $command = sprintf('%s actions:exec "%s:sync:%s"', $consolePath, $this->getProviderName(), $endPoint);
-        shell_exec(sprintf('%s > /dev/null 2>/dev/null &', $command));
+        $fullComand = sprintf('%s > /dev/null 2>/dev/null &', $command);
+
+        shell_exec($fullCommand);
+
+        return false;
     }
 
     /**
